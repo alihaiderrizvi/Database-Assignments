34c1f08badf701dca6dede7b85b8671f41589156
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2020-08-25 22:47:58 +0500

    assignment 1 - initial commit

---
+++ assignment 1/assignment 1/Book Search.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to delete this book?", "Confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                
+            }
+        }
+    }
+}
